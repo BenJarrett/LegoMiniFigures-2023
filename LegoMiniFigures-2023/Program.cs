07820df3343770be407222858f84b229709db62c
BenJarrett <b.cl.jarrett@gmail.com>
2023-12-20 12:29:14 -0500

    Add project files.

---
+++ LegoMiniFigures-2023/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LegoMiniFigures_2023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
